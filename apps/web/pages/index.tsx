807d0f21b01f684029d292e41a0805e0396b2c4a
fervillalbag <21fervillalbag@gmail.com>
2022-08-08 21:04:34 -0400

    remove: store from frontend

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,12 +1,9 @@
 import { Button } from "ui";
-import { useHelloQuery } from "../src/store/services/api";
 
 export default function Web() {
-  const { data } = useHelloQuery();
-
   return (
     <div>
-      <h1>{data?.message}</h1>
+      <h1>Say hello from next</h1>
       <Button />
     </div>
   );
